4dc8159a1a90b77012a7a23a5c7006c5cf52ea14
Nariman Huseynov <hnariman@gmail.com>
2025-08-08 06:07:46 +0400

    feat: structuring & planning tests

--- program/tests/program.ts
+++ program/tests/program.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Blockbuster } from "../target/types/blockbuster";
-import { PublicKey, SystemProgram } from "@solana/web3.js";
+import { Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
 import {
   ASSOCIATED_TOKEN_PROGRAM_ID,
   getAssociatedTokenAddressSync,
@@ -43,8 +43,8 @@ async function setupInitialize() {
   }
 }
 
-describe("init", () => {
-  it("Is initialized!", async () => {
+describe("Actor: Admin", () => {
+  it("Shall be ablet to initialize protocol", async () => {
     const { program, wallet, vault, config, mint } = await setupInitialize();
     const admin = wallet.publicKey;
 
@@ -62,4 +62,58 @@ describe("init", () => {
       .rpc();
     console.log("Your transaction signature", tx);
   });
+
+  describe("Lock:", () => {
+    it("Admin shall be able to lock protocol", () => {
+      throw new Error("Not Implemented");
+    });
+    it("Non-Admin shall not be able to lock protocol", () => {
+      throw new Error("Not Implemented");
+    });
+  })
+  describe("Unlock", () => {
+    it("Admin shall be able to unlock protocol", () => {
+      throw new Error("Not Implemented");
+    });
+    it("Non-admin: shall not be able to unlock protocol", () => {
+      throw new Error("Not Implemented");
+    });
+  })
 });
+
+describe("Actor: Citizen", () => {
+  it("Citizen shall be able to report non existing address", () => {
+    throw new Error("Not Implemented");
+  })
+  it("Citizen shall increase counter & get notified when reporting existing address", () => {
+    throw new Error("Not Implemented");
+  })
+
+  it("Citizen shall not be able to report own address", () => {
+    throw new Error("Not Implemented");
+  })
+  it("Citizen shall not be able to report address in busted list", () => {
+    throw new Error("Not Implemented");
+  })
+  it("Citizen shall not be able to report address in allow-list", () => {
+    throw new Error("Not Implemented");
+  })
+  it("Citizen shall not be able to report anything if citizen is suspect", () => {
+    throw new Error("Not Implemented");
+  })
+  it("Citizen shall not be able to report anything if citizen is busted", () => {
+    throw new Error("Not Implemented");
+  })
+})
+
+describe("Actor: Detective", () => {
+  it("Detective: shall fail to update status without NFT", () => {
+    throw new Error("Not Implemented");
+  })
+  it("Detective: shall be able to update status with NFT", () => {
+    throw new Error("Not Implemented");
+  })
+  it("Detective: shall fail to interact if detective is in suspect list", () => {
+    throw new Error("Not Implemented");
+  })
+})
