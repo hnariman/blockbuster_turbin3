c74c88ff1325ce95fc536e2b466fcacb18db321a
Nariman Huseynov <hnariman@gmail.com>
2025-08-13 06:31:58 +0400

    feat: adding lock to protocol

--- program/programs/program/src/instructions/mod.rs
+++ program/programs/program/src/instructions/mod.rs
@@ -1,9 +1,11 @@
 pub mod consumer_program;
 pub mod detective;
 pub mod initialize;
+pub mod lock;
 pub mod report;
 
 pub use consumer_program::*;
 pub use detective::*;
 pub use initialize::*;
+pub use lock::*;
 pub use report::*;
