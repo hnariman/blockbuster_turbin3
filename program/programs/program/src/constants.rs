c0736db6f3d62a5c9b108a31653cc36b97c460a9
Nariman Huseynov <hnariman@gmail.com>
2025-08-13 06:33:32 +0400

    feat: update constants

--- program/programs/program/src/constants.rs
+++ program/programs/program/src/constants.rs
@@ -1,8 +1,7 @@
 use anchor_lang::prelude::*;
 
 #[constant]
-pub const SEED: &str = "anchor";
-pub const SUSPECT_SEED: &'static [u8; 19] = b"blockbuster_suspect";
 pub const MAX_REPORTS: u8 = 200;
+pub const SUSPECT_SEED: &'static [u8; 19] = b"blockbuster_suspect";
 pub const CONF_SEED: &'static [u8; 18] = b"blockbuster_config";
 pub const MINT_SEED: &'static [u8; 16] = b"blockbuster_mint";
